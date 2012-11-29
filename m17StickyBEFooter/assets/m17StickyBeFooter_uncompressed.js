712dcc7773c2f9082c612dcc5be67df9c946e31b
Joe Ray Gregory <j.r.gregory@gmx.de>
2012-11-29 22:26:53 +0100

    Added the right file for the commit before =_0

--- m17StickyBEFooter/assets/m17StickyBeFooter_uncompressed.js
+++ m17StickyBEFooter/assets/m17StickyBeFooter_uncompressed.js
@@ -1,34 +1,29 @@
 /* Stcky Footer for Contao
  * @author: Joe Ray Gregory aka may17
+ * @author: Stefan Melz (Debugbar bugfix)
  * @copyright: Joe Ray Gregory 2012 joe@may17.de
  * @license LGPL
  */
 (function() {
-    var M17StickyFooter = {
+    M17StickyFooter = {
         init: function() {
+
             var formBody = document.getElement('.tl_formbody_submit') || false,
-                pos,
-                formHeight,
-                tt,
-                formBodyPos,
-                difference,
                 setEvent,
-                fieldsetEl,
+                fieldsets,
+                debugToggler,
                 self = this;
 
             //invoke only when the element existists
             if(formBody) {
-                formBodyPos = formBody.getPosition;
-                formHeight = formBody.getHeight();
-                console.log(formHeight);
-                tt = formBodyPos.y + 49;
-                pos = formBody.getPosition().y + formBody.getHeight();
 
+            	this.footerData = this.generateSubmitObj(formBody);
+				this.generateOffset();
                 //load the methoad with init
-                this.setItem(formBody);
+                this.setItem();
                 
                	//Prepare event
-               	setEvent = this.setItem.bind(this).pass(formBody);
+               	setEvent = this.setItem.bind(this);
                	
                	//Add resize and scroll events
                 window.addEvents({
@@ -37,38 +32,77 @@
                     'ajax_change': setEvent
                 });
                 
+                fieldsets = $$('fieldset');
+                
                 //addEvent Delegation for legends
-                fieldsetEl = $$('fieldset');
-                fieldsetEl.addEvent('click:relay(legend)', function(e){
-                    self.setItem(formBody);
+                fieldsets.addEvent('click:relay(legend)', function(e){
+                    self.setItem();
                 });
                 
                 //Improvment for checkbox actions
-                fieldsetEl.addEvent('click:relay(input.tl_checkbox[onclick])', function(e){
-                    self.setItem(formBody);
+                fieldsets.addEvent('click:relay(input.tl_checkbox[onclick])', function(e){
+                    self.setItem();
                 });
+                
+                //Watch debug toggler
+                debugToggler = document.id('tog') || false;
+                if(debugToggler) {
+                    debugToggler.addEvent('click',function(){
+                    	self.setItem();
+                	})
+                }
             }
         },
+        
+        /* generateSubmitObj
+         * @desc generates the internal footer Object
+         */
+        generateSubmitObj: function(formbody) {
+        	var submitContainer = formbody.getFirst();
+        	
+        	return {
+        		item: formbody,
+        		submitContainer: submitContainer,
+        		height: submitContainer.getHeight(),
+        		width: submitContainer.getStyle('width'),
+        		offset: 0
+        	}
+        },
+        
+        /* generateOffset
+         * @desc calculates an offset if debug bar is active
+         */
+        generateOffset: function(){
+			var debug = document.id('debug') || false;
+			
+			
+            //set news offset for debugbar
+            if(debug){
+                var offset = debug.getElement('p').getSize().y + debug.getElement('div').getSize().y -1;
+                    this.footerData.submitContainer.setStyle('bottom', offset);
+                    this.footerData.offset = offset;
+            }
+        },
+        
         /* initStickyFooter
          * @desc initialize the stickyFooter for Backend
          */
         setItem: function(formBody) {
-            var raffle = formBody.getPosition().y +49,
-            	diff = raffle - window.getHeight(),
-            	formBodyInner = formBody.getFirst(),
-            	_width = formBodyInner.getStyle('width'),
-				$this = window,
-                $body = document.body,
+            var offset = this.generateOffset(),
+            	raffle = this.footerData.item.getPosition().y + this.footerData.height + this.footerData.offset,
+            	win = window,
+            	diff = raffle - win.getHeight(),
+                docBody = document.body,
                 stickyClass = 'stickySave',
-            	scrollSizeToInt = $this.getScroll().y.toInt();
+            	scrollSizeToInt = win.getScroll().y.toInt();
             	
             if(scrollSizeToInt > diff) {
-                $body.removeClass(stickyClass);
+                docBody.removeClass(stickyClass);
             } else {
-                //check if class is set
-                if(!$body.hasClass(stickyClass)) {
-                    formBodyInner.setStyle('width', _width);
-                    $body.addClass(stickyClass);
+                //check if class set
+                if(!docBody.hasClass(stickyClass)) {
+                    this.footerData.submitContainer.setStyle('width', this.footerData.width);
+                    docBody.addClass(stickyClass);
                 }
             }
         }
